28634e6e70e54ae0ca148979625f20d28d9b5196
osalem192 <124590406+osalem192@users.noreply.github.com>
2023-02-12 03:25:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -14,31 +14,59 @@
 #include <util/delay.h>
 
 int main(void)
-{	uint8 arr[] = {0b00111111,0b00110000,0b001011011,0b01001111,0b01100110,0b01101101,0b01111101,0b00000111,0b01111111,0b01101111};
+{	Set_Port_Dir('b',1);
 	Set_Port_Dir('c',1);
 	Set_Port_Dir('d',1);
 	Set_Port_Dir('a',0);
-    uint8 i =1;
+//     uint8 i =1;             // for the first functoin
 	while (1)
 	{
-// 		 uint8 x = Button_state('a',0);
-// 		if(x == 1){
-// 			{
-// 				_delay_ms(300);
-// 				Port_Write('c',Seven_Seg_Write(i));
-// 			}
-// 			i++;
-// 			
-// 			if(i==10) i=0;
-// 		}
-		
+// the code below for button counter		 
+
+// 			if(Button_state('a',0) == 1){
+//				{
+// 						_delay_ms(300);
+//  						Seven_Seg_Write('c',i);
+// 					}
+// 					i++;
+// 					
+// 					if(i==10) i=0;
+// 				}
+				
 // the code below makes the animation		
 	
-			for (uint8 i=0 ; i < 6; i++)
-			{
-				Pin_Write('c',i,1);
-				_delay_ms(100);
-			}
-			Port_Write('c',0);
+// 			for (uint8 i=0 ; i < 6; i++)
+// 			{
+// 				Pin_Write('c',i,1);
+// 				_delay_ms(100);
+// 			}
+// 			Port_Write('c',0);
+
+// the code below counts from 0 to 9 then from 9 to 0 
+
+// 			for (uint8 i =0 ; i < 10; i++)
+// 			{
+// 				Seven_Seg_Write('c',i);
+// 				_delay_ms(250);
+// 			}
+// 
+// 			for (sint8 i = 9; i > 0; i--)
+// 			{
+// 				Seven_Seg_Write('c',i);
+// 				_delay_ms(250);
+// 			}
+
+// the code below display numbers from 0:9
+
+//			Seven_Seg_Write_1_to_9('c');
+
+//  the code below display numbers from 0:99 
+
+// 			Seven_Seg_Write_1_to_99('d','c');
+
+// the code below use BCD 
+
+//			Seven_Segment_4BCD('b',9);			
+
 	}
 }
