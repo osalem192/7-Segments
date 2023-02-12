38dff0d1d5aa3284e3d2fb240105b76447dff7b2
osalem192 <124590406+osalem192@users.noreply.github.com>
2023-02-12 03:26:47 +0200

    Update 7_Seg.h

--- 7_Seg.h
+++ 7_Seg.h
@@ -10,8 +10,11 @@
 #define _7_SEG_H_
 
 void Seven_Seg_Init(uint8 port);
+void Seven_Seg_Write(uint8 port,uint8 i);
+void Seven_Seg_Write_1_to_9(uint8 port);
+void Seven_Seg_Write_1_to_99(uint8 port1, uint8 port2);
+void Seven_Segment_4BCD(uint8 port, uint8 number);
 
-uint8 Seven_Seg_Write(uint8 i);
 
 
 
